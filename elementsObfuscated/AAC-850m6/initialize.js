54126cf29bd5122c4ecc55aebea81beb71fedee1
zqplugins <137153217+zqplugins@users.noreply.github.com>
2024-02-14 02:32:52 +0200

    Automatic commit from Bubble

--- elementsObfuscated/AAC-850m6/initialize.js
+++
@@ -1,5 +0,0 @@
-function(instance, context){
-	function initialize(_0x3eaaff, _0x267a90) {
-}
-	 initialize(instance, context);
-}
--- elementsObfuscated/AAC-850m6/reset.js
+++
@@ -1,5 +0,0 @@
-function(instance, context){
-	function reset(_0x3832b8, _0x139a0e) {
-}
-	 reset(instance, context);
-}
